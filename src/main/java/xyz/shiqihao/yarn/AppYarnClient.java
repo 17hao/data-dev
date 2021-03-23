22b4b65817847b562080211269b26092c9b731b0
17hao <sqh1107@gmail.com>
2021-03-23 23:13:39 +0800

    yarn application update

--- src/main/java/xyz/shiqihao/yarn/AppYarnClient.java
+++ src/main/java/xyz/shiqihao/yarn/AppYarnClient.java
@@ -1,35 +1,84 @@
 package xyz.shiqihao.yarn;
 
-import org.apache.commons.cli.*;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.yarn.api.ApplicationConstants;
+import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationResponse;
+import org.apache.hadoop.yarn.api.records.*;
 import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.client.api.YarnClientApplication;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Client <--> ResourceManager  YarnClient
+ * ApplicationMaster <--> ResourceManger AMRMClient
+ * ApplicationMaster <--> NodeManager NMClient
+ */
 public class AppYarnClient {
-    private Configuration conf;
-    private YarnClient client;
-    private Options opts;
+    public static void main(String... args) throws IOException, YarnException {
+        int amMemory = 512;
 
-    public AppYarnClient() {
-        conf = new YarnConfiguration();
-        client = YarnClient.createYarnClient();
-        client.init(conf);
-        opts = new Options();
+        Configuration conf = new YarnConfiguration();
+        YarnClient yarnClint = YarnClient.createYarnClient();
+        yarnClint.init(conf);
+        yarnClint.start();
+        YarnClientApplication app = yarnClint.createApplication();
+        // appResponse contains information about the cluster such as max/min resource capabilities of the cluster
+        GetNewApplicationResponse appResponse = app.getNewApplicationResponse();
 
-        opts.addOption("appname", true, "Application Name");
-        opts.addOption("jar", true, "JAR file containing the application");
-    }
+        // set the application submission context
+        ApplicationSubmissionContext appContext = app.getApplicationSubmissionContext();
+        ApplicationId appId = appContext.getApplicationId();
 
-    public static void main(String[] args) {
-        AppYarnClient client = new AppYarnClient();
-        CommandLineParser parser = new DefaultParser();
-        try {
-            CommandLine line = parser.parse(client.opts, args);
-        } catch (ParseException e) {
-            e.printStackTrace();
+        appContext.setApplicationName("my app");
+
+        // set local resources for the application master
+        Map<String, LocalResource> localResources = new HashMap<>();
+
+        // set the env variables to be setup in the env where application master will be run
+        Map<String, String> env = new HashMap<>();
+
+        List<String> vargs = new ArrayList<>();
+        vargs.add(ApplicationConstants.Environment.JAVA_HOME.$$() + "/bin/java");
+        vargs.add("-Xmx" + amMemory + "m");
+        vargs.add("xyz.shiqihao.yarn.ApplicationMaster");
+        vargs.add("1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/AppMaster.stdout");
+        vargs.add("2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/AppMaster.stderr");
+
+        StringBuilder command = new StringBuilder();
+        for (String s : vargs) {
+            command.append(s).append(" ");
         }
 
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("my app", client.opts);
+        // application master command
+        List<String> commands = new ArrayList<>();
+        commands.add(command.toString());
+
+        FileSystem fs = FileSystem.get(conf);
+
+        // set up the container launch context for the application master
+        ContainerLaunchContext amContainer = ContainerLaunchContext.newInstance(localResources, env, commands, null, null, null);
+
+        // set up resources type requirements
+        Resource capacity = Resource.newInstance(amMemory, 1);
+        appContext.setResource(capacity);
+
+        appContext.setAMContainerSpec(amContainer);
+
+        // set the priority for the application master
+        Priority priority = Priority.newInstance(1);
+        appContext.setPriority(priority);
+
+        // set the queue to which this application is to be submitted in th RM
+        appContext.setQueue("default");
+
+        yarnClint.submitApplication(appContext);
     }
 }
--- src/main/java/xyz/shiqihao/yarn/ApplicationMaster.java
+++ src/main/java/xyz/shiqihao/yarn/ApplicationMaster.java
@@ -1,4 +1,75 @@
 package xyz.shiqihao.yarn;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.api.protocolrecords.RegisterApplicationMasterResponse;
+import org.apache.hadoop.yarn.api.records.Container;
+import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
+import org.apache.hadoop.yarn.client.api.AMRMClient;
+import org.apache.hadoop.yarn.client.api.async.AMRMClientAsync;
+import org.apache.hadoop.yarn.client.api.async.NMClientAsync;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.hadoop.yarn.util.Records;
+
+import java.io.IOException;
+
+
 public class ApplicationMaster {
+    private final Configuration conf;
+
+    private final AMRMClientAsync amRMClient;
+    private final NMClientAsync nmClient;
+    private final AMRMClientAsync.AbstractCallbackHandler allocatorHandler;
+    private final NMClientAsync.AbstractCallbackHandler containerListener;
+
+    private final String appHostName = "aliyun";
+    private final int numTotalContainers = 2;
+    private final int containerMemory = 512;
+    private final String appJar = "~/xxx.jar";
+    private final int priority = 0;
+
+    public ApplicationMaster() {
+        conf = new YarnConfiguration();
+        allocatorHandler = new RMCallbackHandler();
+        amRMClient = AMRMClientAsync.createAMRMClientAsync(1000, allocatorHandler);
+        containerListener = new NMCallbackHandler();
+        nmClient = NMClientAsync.createNMClientAsync(containerListener);
+    }
+
+    public static void main(String[] args) {
+    }
+
+    private void run() throws IOException, YarnException {
+        amRMClient.init(conf);
+        amRMClient.start();
+
+        nmClient.init(conf);
+        nmClient.start();
+
+        RegisterApplicationMasterResponse response = amRMClient.registerApplicationMaster("", 9999, "google.com");
+
+        long maxMem = response.getMaximumResourceCapability().getMemorySize();
+
+        for (int i = 0; i < numTotalContainers; i++) {
+            AMRMClient.ContainerRequest containerRequest = AMRMClient.ContainerRequest.newBuilder().build();
+            amRMClient.addContainerRequest(containerRequest);
+        }
+    }
+
+    private class LaunchContainerRunnable implements Runnable {
+        Container container;
+        NMCallbackHandler containerListener;
+
+        LaunchContainerRunnable(Container container, NMCallbackHandler containerListener) {
+            this.container = container;
+            this.containerListener = containerListener;
+        }
+
+        @Override
+        public void run() {
+            ContainerLaunchContext ctx = Records.newRecord(ContainerLaunchContext.class);
+            //containerListener.container
+            nmClient.startContainerAsync(container, ctx);
+        }
+    }
 }
